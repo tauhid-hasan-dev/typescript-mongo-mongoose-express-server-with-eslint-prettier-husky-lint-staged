8d72ced13b59284aa9e251f2e0b0d7e9a438532f
Tauhid Hasan <tauhidhasan11@gmail.com>
2023-06-01 19:36:30 +0300

    [Done] cors and parser setup completed

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,16 @@
 import express, { Application } from 'express'
+import cors from 'cors'
 const app: Application = express()
 const port = 3000
 
+// cors
+app.use(cors())
+
+// parser 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }))
+
+// Testing 
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
