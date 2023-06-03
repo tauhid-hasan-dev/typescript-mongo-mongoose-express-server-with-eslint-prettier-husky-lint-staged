cc61595287cd52cf420f0612a641ecf9a6f18556
Tauhid Hasan <tauhidhasan11@gmail.com>
2023-06-03 13:33:57 +0300

    so alhamdulillah eslint, prettier, husky and lint-staged is installed correctly and working fine

--- src/app.ts
+++ src/app.ts
@@ -10,7 +10,7 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
 // Testing
-app.get('/', (req: Request, res: any) => {
+app.get('/', (req: Request, res: Response) => {
   res.send('Hello World!')
 })
 
